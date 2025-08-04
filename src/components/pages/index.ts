ffd18de9a5f348aaac21e11736b2eced95d02443
VanTuoi <vantuoi.dev@gmail.com>
2025-08-04 12:37:34 +0700

    chore: fix build

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -2,6 +2,6 @@ export * from "./about";
 export * from "./cart";
 export * from "./check-out";
 export * from "./contact";
-export * from "./home-page";
+// export * from "./home-page";
 export * from "./product";
 export * from "./wish-list";
